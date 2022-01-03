a09c886feb2fd297eeebf57f2ed010abd1c79fb5
kySuga <kyle@masuga.com>
2022-01-02 23:11:02 -0500

    Initial commit - Running through next.js tutorial.

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+// added file to add lang to html tag. A bit confused still on if this setup because of 'caveats' mentioned on this doc: https://nextjs.org/docs/advanced-features/custom-document
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
